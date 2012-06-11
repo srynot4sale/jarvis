5057e000491226878092fc04c867c43f23b2e404
Aaron Barnes <aaron.barnes@hbcosmo.com>
2012-06-11 12:27:20 +1200

    In debug mode, do not create a daemon for server

--- start_server.py
+++ start_server.py
@@ -11,7 +11,6 @@ import config
 # kernel.setConfig('data_password',         'password')
 # kernel.setConfig('interface_http_port',   'XXXX')
 # kernel.setConfig('username',              'My Name')
-# kernel.setConfig('debuglogfile',          '/path/to/logfile')
 #
 
 ## Initialise Jarvis kernel
@@ -20,10 +19,7 @@ import kernel
 import sys
 
 if len(sys.argv) > 1 and sys.argv[1] == 'debug':
-    stderr = file(kernel.getConfig('debuglogfile'), 'w+')
-
-    with daemon.DaemonContext(stderr=stderr, stdout=stderr):
-        kernel.init()
+    kernel.init()
 else:
     with daemon.DaemonContext():
         kernel.init()
