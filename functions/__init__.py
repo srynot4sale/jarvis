e0f2bfcf493afb365e2969634904e69b0852fc0f
Aaron Barnes <aaron@io.net.nz>
2013-04-28 23:08:59 +1200

    functions/tv.py: Remove module

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,13 +2,11 @@
 import functions.server
 import functions.calendar
 import functions.list
-import functions.tv
 
 def init():
     funcs = []
     funcs.append(functions.server.init())
     funcs.append(functions.calendar.init())
     funcs.append(functions.list.init())
-    funcs.append(functions.tv.init())
 
     return funcs
