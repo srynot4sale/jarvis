6965eaf62e3bb077133ccfab1f7acc76e9141294
Aaron Barnes <aaron@io.net.nz>
2014-02-24 10:57:30 +1300

    cron.py: Only print output on error

--- cron.py
+++ cron.py
@@ -4,4 +4,8 @@ from clients.http import make_request
 
 res = make_request('server cron', prodok=True)
 
-print(repr(res))
+del res['state']
+
+# Only output on error
+if 'state' not in res or res['state'] != 1:
+    print(repr(res))
