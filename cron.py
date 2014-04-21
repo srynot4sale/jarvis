c0e2134a10923cffad4f84ec1f6c7ddf4ab9b248
Aaron Barnes <aaron@io.net.nz>
2014-04-21 13:34:08 +1200

    cron.py: Bug in cron.py meant it didn't suppress output on success

--- cron.py
+++ cron.py
@@ -4,8 +4,6 @@ from clients.http import make_request
 
 res = make_request('server cron', prodok=True)
 
-del res['state']
-
 # Only output on error
 if 'state' not in res or res['state'] != 1:
     print(repr(res))
