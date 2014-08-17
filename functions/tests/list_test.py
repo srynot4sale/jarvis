cadbb6bbc100cde2e9dbda572b309be4ebef3c26
Alastair Munro <aperson39@gmail.com>
2014-08-17 16:39:17 +1200

    list_test.py: Remove magic numbers from tests

--- functions/tests/list_test.py
+++ functions/tests/list_test.py
@@ -2,11 +2,16 @@ from clients.http import make_nonprod_request as make_request
 from nose.tools import with_setup
 import test
 
+STATE_SUCCESS = 1       # Response completed succesfully
+STATE_FAILURE = 2       # Response failed due to user error
+STATE_PANIC   = 3       # Response failed due to system error
+STATE_AUTHERR = 4       # Response failed due to authentication error
+
 # Utility functions
 # Reset a list by deleting all items
 def list_empty(tag):
     exist = make_request('list view %s' % tag)
-    if exist['state'] == 2:
+    if exist['state'] == STATE_FAILURE:
         return
 
     for item in exist['data']:
@@ -44,16 +49,16 @@ def badinput_test():
         # check for empty list first
         list_empty(b)
         empty = make_request('list view %s' % b)
-        assert empty['state'] == 2
+        assert empty['state'] == STATE_FAILURE
         empty = None
 
         newitem = make_request('list add %s %s' % (b, good))
-        assert newitem['state'] == 1
+        assert newitem['state'] == STATE_SUCCESS
         newitem = None
 
         # check new item exists
         exists = make_request('list view %s' % b)
-        assert exists['state'] == 1
+        assert exists['state'] == STATE_SUCCESS
         assert len(exists['data']) == 1
         if exists['data'][0][0] != good:
             raise Exception(b)
@@ -63,12 +68,12 @@ def badinput_test():
 
         # delete list item
         delete = make_request(exists_delete)
-        assert delete['state'] == 1
+        assert delete['state'] == STATE_SUCCESS
         delete = None
 
         # check list is empty again
         empty2 = make_request('list view %s' % b)
-        assert empty2['state'] == 2
+        assert empty2['state'] == STATE_FAILURE
         empty2 = None
 
 @with_setup(test.setup_function, test.teardown_function)
@@ -82,19 +87,19 @@ def list_adddeleteitem_test():
     # check for empty list first
     list_empty(tag)
     empty = make_request('list view %s' % tag)
-    assert empty['state'] == 2
-    assert empty['write'] == 0
+    assert empty['state'] == STATE_FAILURE
+    assert empty['write'] == False
     empty = None
 
     # add new item
     newitem = make_request('list add %s %s' % (tag, listitem))
-    assert newitem['state'] == 1
-    assert newitem['write'] == 1
+    assert newitem['state'] == STATE_SUCCESS
+    assert newitem['write'] == True
     newitem = None
 
     # check new item exists
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists_delete = exists['data'][0][2]['Delete']
@@ -102,13 +107,13 @@ def list_adddeleteitem_test():
 
     # delete list item
     delete = make_request(exists_delete)
-    assert delete['state'] == 1
-    assert delete['write'] == 1
+    assert delete['state'] == STATE_SUCCESS
+    assert delete['write'] == True
     delete = None
 
     # check list is empty again
     empty2 = make_request('list view %s' % tag)
-    assert empty2['state'] == 2
+    assert empty2['state'] == STATE_FAILURE
     empty2 = None
 
 @with_setup(test.setup_function, test.teardown_function)
@@ -126,12 +131,12 @@ def list_adddeletetags_test():
 
     # add new item with first tag
     newitem = make_request('list add %s %s' % (tag, listitem))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # check new item exists
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     existsid = exists['data'][0][2]['Delete'].split(' ')[3]
@@ -139,23 +144,23 @@ def list_adddeletetags_test():
 
     # add second tag
     newtag = make_request('list tag %s %s' % (existsid, tag2))
-    assert newtag['state'] == 1
+    assert newtag['state'] == STATE_SUCCESS
     newtag = None
 
     # check new item exists
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists = make_request('list view %s' % tag2)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists = None
 
     # delete list item
     delete = make_request('list remove %s %s' % (tag, existsid))
-    assert delete['state'] == 1
+    assert delete['state'] == STATE_SUCCESS
     assert len(delete['data']) == 2
     assert delete['data'][0][0] == 'View list "%s"' % tag
     assert delete['data'][1][0] == 'View list "%s"' % tag2
@@ -163,19 +168,19 @@ def list_adddeletetags_test():
 
     # already deleted
     delete = make_request('list remove %s %s' % (tag, existsid))
-    assert delete['state'] == 2
+    assert delete['state'] == STATE_FAILURE
     delete = None
 
     delete = make_request('list remove %s %s' % (tag2, existsid))
-    assert delete['state'] == 1
+    assert delete['state'] == STATE_SUCCESS
     assert len(delete['data']) == 1
     delete = None
 
     # check list is empty again
     empty2 = make_request('list view %s' % tag)
-    assert empty2['state'] == 2
+    assert empty2['state'] == STATE_FAILURE
     empty2 = make_request('list view %s' % tag2)
-    assert empty2['state'] == 2
+    assert empty2['state'] == STATE_FAILURE
     empty2 = None
 
 @with_setup(test.setup_function, test.teardown_function)
@@ -192,12 +197,12 @@ def list_adddeletetags_test():
 
     # add new item with first tag
     newitem = make_request('list add %s %s' % (tag, listitem))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # check new item exists
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     existsid = exists['data'][0][2]['Delete'].split(' ')[3]
@@ -205,31 +210,31 @@ def list_adddeletetags_test():
 
     # add same tag again
     newtag = make_request('list tag %s %s' % (existsid, tag))
-    assert newtag['state'] == 1
+    assert newtag['state'] == STATE_SUCCESS
     newtag = None
 
     # check item only shows once
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists = None
 
     # delete list item
     delete = make_request('list remove %s %s' % (tag, existsid))
-    assert delete['state'] == 1
+    assert delete['state'] == STATE_SUCCESS
     assert len(delete['data']) == 1
     assert delete['data'][0][0] == 'View list "%s"' % tag
     delete = None
 
     # check it no longer appears
     delete = make_request('list remove %s %s' % (tag, existsid))
-    assert delete['state'] == 2
+    assert delete['state'] == STATE_FAILURE
     delete = None
 
     # check list is empty again
     empty2 = make_request('list view %s' % tag)
-    assert empty2['state'] == 2
+    assert empty2['state'] == STATE_FAILURE
     empty2 = None
 
 
@@ -247,12 +252,12 @@ def list_itemorder_basic_test():
     # add four new items
     for i in range(0, 4):
         newitem = make_request('list add %s %s' % (tag, '%s%d' % (listitem, i)))
-        assert newitem['state'] == 1
+        assert newitem['state'] == STATE_SUCCESS
         newitem = None
 
     # check four items exist
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 4
 
     # check they are in the same order we added them
@@ -281,11 +286,11 @@ def list_itemorder_multitag_test():
 
     # add item to alternate list first
     newitem = make_request('list add %s %s' % (tagalt, '%s%d' % (listitem, 9)))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     exists = make_request('list view %s' % tagalt)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     altitemid = exists['data'][0][2]['Delete'].split(' ')[3]
     exists = None
@@ -293,23 +298,23 @@ def list_itemorder_multitag_test():
     # add two new items to correct tag
     for i in range(0, 2):
         newitem = make_request('list add %s %s' % (tag, '%s%d' % (listitem, i)))
-        assert newitem['state'] == 1
+        assert newitem['state'] == STATE_SUCCESS
         newitem = None
 
     # check two items exist
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 2
     exists = None
 
     # add tag to alternate item, adding it to the *end* of this list
     newitem = make_request('list tag %s %s' % (altitemid, tag))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # check three items exist
     exists = make_request('list view %s' % tag)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 3
 
     # check they are in the same order we added the tags
@@ -332,12 +337,12 @@ def list_missingtag_test():
     Test handling of empty tags
     '''
     newitem = make_request('list add  item')
-    assert newitem['state'] == 2
+    assert newitem['state'] == STATE_FAILURE
     assert newitem['message'] == 'No tag specified'
     newitem = None
 
     newitem = make_request('list tag 1 ')
-    assert newitem['state'] == 2
+    assert newitem['state'] == STATE_FAILURE
     assert newitem['message'] == 'No tag specified'
     newitem = None
 
@@ -357,12 +362,12 @@ def list_move_test():
 
     # add new item with first tag
     newitem = make_request('list add %s %s' % (tag_origin, listitem))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # check new item exists
     exists = make_request('list view %s' % tag_origin)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     existsid = exists['data'][0][2]['Delete'].split(' ')[3]
@@ -370,41 +375,41 @@ def list_move_test():
 
     # fail to move item from wrong tag
     wrongtag = make_request('list move %s %s %s' % (existsid, 'UNITESTNONEXISTANTTAG', tag_dest))
-    assert wrongtag['state'] == 2
+    assert wrongtag['state'] == STATE_FAILURE
     wrongtag = None
 
     # move item to new tag
     newtag = make_request('list move %s %s %s' % (existsid, tag_origin, tag_dest))
-    assert newtag['state'] == 1
+    assert newtag['state'] == STATE_SUCCESS
     newtag = None
 
     # check item only shows once
     exists = make_request('list view %s' % tag_dest)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists = None
 
     # check it no longer appears at old tag
     old = make_request('list view %s' % (tag_origin))
-    assert old['state'] == 2
+    assert old['state'] == STATE_FAILURE
     old = None
 
     # delete list item
     delete = make_request('list remove %s %s' % (tag_dest, existsid))
-    assert delete['state'] == 1
+    assert delete['state'] == STATE_SUCCESS
     assert len(delete['data']) == 1
     assert delete['data'][0][0] == 'View list "%s"' % tag_dest
     delete = None
 
     # check list is empty again
     empty2 = make_request('list view %s' % tag_origin)
-    assert empty2['state'] == 2
+    assert empty2['state'] == STATE_FAILURE
     empty2 = None
 
     # check list is empty again
     empty3 = make_request('list view %s' % tag_dest)
-    assert empty3['state'] == 2
+    assert empty3['state'] == STATE_FAILURE
     empty3 = None
 
 
@@ -424,40 +429,40 @@ def list_multipletagview_test():
 
     # add new item with first tag
     newitem = make_request('list add %s %s' % (tag_one, listitem))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     itemid = newitem['data'][0][2]['Delete'].split(' ')[3]
     newitem = None
 
     # add new item with first tag
     newitem = make_request('list add %s %s' % (tag_one, listitemsingle))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # add new item with second tag
     newitem = make_request('list add %s %s' % (tag_two, listitemsingle))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # add second tag to first item
     newitem = make_request('list tag %s %s' % (itemid, tag_two))
-    assert newitem['state'] == 1
+    assert newitem['state'] == STATE_SUCCESS
     newitem = None
 
     # check two items in first tag
     exists = make_request('list view %s' % tag_one)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 2
     exists = None
 
     # check one item in second tag
     exists = make_request('list view %s' % tag_two)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 2
     exists = None
 
     # check one item when loading both tags!
     exists = make_request('list view %s %s' % (tag_one, tag_two))
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists = None
@@ -477,13 +482,13 @@ def list_addmoveitem_test():
 
     # Add new item
     newitem = make_request('list add %s %s' % (tag1, listitem))
-    assert newitem['state'] == 1
-    assert newitem['write'] == 1
+    assert newitem['state'] == STATE_SUCCESS
+    assert newitem['write'] == True
     newitem = None
 
     # check new item exists
     exists = make_request('list view %s' % tag1)
-    assert exists['state'] == 1
+    assert exists['state'] == STATE_SUCCESS
     assert len(exists['data']) == 1
     assert exists['data'][0][0] == listitem
     exists_delete = exists['data'][0][2]['Delete']
@@ -492,16 +497,16 @@ def list_addmoveitem_test():
 
     # Move new item
     move = make_request('list move %s %s %s' % (itemid, tag1, tag2))
-    assert move['state'] == 1
+    assert move['state'] == STATE_SUCCESS
     assert len(move['data']) == 1
     assert move['data'][0][0] == listitem
 
     # Check to see if tag1 list is empty
     empty = make_request('list view %s' % tag1)
-    assert empty['state'] == 2
+    assert empty['state'] == STATE_FAILURE
 
     # Make sure item is in list 2
     moved = make_request('list view %s' % tag2)
-    assert moved['state'] == 1
+    assert moved['state'] == STATE_SUCCESS
     assert len(moved['data']) == 1
     assert moved['data'][0][0] == listitem
