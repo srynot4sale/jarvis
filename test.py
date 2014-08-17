b57131800e6dfda4ca237bab5b4733df1cecaf88
Aaron Barnes <aaron@io.net.nz>
2014-08-17 15:47:28 +1200

    test.py: Add a very basic test coverage utility

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import subprocess
-import time
-import logging
+import logging, os.path, re, subprocess, time
+
+## Test suite setup and teardown functionality
 log = logging.getLogger(__name__)
 
 server = None
@@ -28,3 +28,34 @@ def setup_function():
 def teardown_function():
     log.info('Run teardown')
     server.kill()
+
+
+## Test coverage
+def test_coverage():
+    matches = subprocess.check_output(["""grep -RE -o 'class action_(.*)\(' functions/ | grep -vi test"""], shell=True)
+    for match in matches.split('\n'):
+        if not match.strip():
+            continue
+        match = re.search('functions/([a-z0-9]+)\.py\:class action_([a-z0-9_]+)\(', match)
+        function = match.group(1)
+        action = match.group(2)
+
+        testfile = 'functions/tests/%s_test.py' % function
+        if not os.path.isfile(testfile):
+            print('%s %s - No test file exists! Expecting %s' % (function, action, testfile))
+            continue
+
+        try:
+            tests = subprocess.check_output(["""grep -RE -o 'def %s_%s(_[a-z0-9_]+_|_)test\(' %s""" % (function, action, testfile)], shell=True)
+            count = len(tests.strip().split('\n'))
+        except:
+            count = 0
+
+        print('%s %s - %d tests' % (function, action, count))
+
+
+
+if __name__ == "__main__":
+    print("Run test coverage:")
+    print('')
+    test_coverage()
