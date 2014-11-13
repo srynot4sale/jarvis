b7d99af708571a63b544fee8fb4b71c639ca4d33
Aaron Barnes <aaron@io.net.nz>
2014-11-13 21:43:42 +1300

    test.py: Log test server output to aid with debugging

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-import logging, os.path, re, subprocess, time
+import logging, os.path, re, subprocess, tempfile, time
 
 import clients.http
 
 ## Test suite setup and teardown functionality
 log = logging.getLogger(__name__)
 
+# Open file descriptor for a unique temp file so we can pipe stdout and sterr to it
+serveroutput, serveroutput_path = tempfile.mkstemp(suffix='.log', prefix='jarvis_test_output_', text=True)
+
 server = None
 def setup_function():
     global server
     log.info('Run setup')
-
-    # Open file descriptor for /dev/null so we can pipe stdout and sterr to it
-    null = open('/dev/null', 'w')
+    log.info('Server output piped to %s' % serveroutput_path)
 
     # Open test instance of server, which resets database on load
-    server = subprocess.Popen(["python", "start_server.py", "--test"], stdout=null, stderr=null)
+    server = subprocess.Popen(["python", "start_server.py", "--test"], stdout=serveroutput, stderr=subprocess.STDOUT)
 
     # The server takes a while to come up, so we'll hit it a few times until we get a proper response
     tries = 0
