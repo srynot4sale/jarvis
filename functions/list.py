15590840931fc395f72e70e087275eba21b853bd
Aaron Barnes <aaron@io.net.nz>
2015-01-18 21:19:56 +1300

    functions/list.py: Add "list random #tag" function

--- functions/list.py
+++ functions/list.py
@@ -1,4 +1,5 @@
 # Jarvis list function
+import random
 import re
 
 import function
@@ -616,6 +617,26 @@ class action_history(kernel.action.action):
             data.append([item['item'], None, item_actions])
 
         return function.response(function.STATE_SUCCESS, 'List previous versions of "%s" with id "%s"' % (itemdata['item'], itemdata['id']), data)
+
+
+class action_random(kernel.action.action):
+
+    usage = '$listkey [$listkey ...]'
+
+    def execute(self, tags):
+        # tags in this case is a list of all the tags supplied
+        # tagstr is the tags imploded around whitespace
+        tags = [normalise_tag(t) for t in tags]
+        tagstr = ' '.join(tags)
+        l = lstobj(self.function, tags)
+
+        items = l.get_all()
+        ritem = random.choice(items)
+
+        data = []
+        data.append([ritem['item'], None])
+
+        return function.response(function.STATE_SUCCESS, 'Random item tagged with "%s"' % tagstr, data)
 
 
 class action_default(action_list):
