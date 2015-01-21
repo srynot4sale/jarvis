0325bcb53ea34516dad09f75190a1898dcfd90a6
Aaron Barnes <aaron@io.net.nz>
2015-01-21 14:40:15 +1300

    clients/http.py: Remove old make_nonprod_request() method

--- clients/http.py
+++ clients/http.py
@@ -20,23 +20,3 @@ def make_request(request, altsecret = None):
 
     response = requests.get(url, headers={'secret': secret})
     return json.loads(response.text)
-
-
-def make_nonprod_request(request, altsecret = None):
-
-    # Get test config
-    test_config = config.config
-    test_config.update(config.config_test)
-
-    baseurl = test_config['web_baseurl']
-
-    secret  = test_config['secret']
-    if altsecret:
-        secret = altsecret
-
-    command = urllib.quote(request, '')
-    uri = command.replace(urllib.quote(' '), '/', 2)
-    url = baseurl + 'api/' + uri
-
-    response = requests.get(url, headers={'secret': secret})
-    return json.loads(response.text)
